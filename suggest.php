8eeb1f52b7423eac4183c80ac1d8c517d5f5cde1
Trent McDole <trent@southernviewmedia.com>
2018-04-06 16:41:23 -0500

    trying to get the email form to send from local server

--- suggest.php
+++ suggest.php
@@ -4,6 +4,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/phpmailer/src/PHPMailer.php';
 require 'vendor/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/src/SMTP.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$name = trim(filter_input(INPUT_POST,"name",FILTER_SANITIZE_STRING));
@@ -30,14 +31,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	//To Do: Send Email
 	$mail = new PHPMailer;
-        // $mail->isSMTP();
-        // $mail->Host = 'localhost';
-        // $mail->Port = 80;
-        // $mail->CharSet = 'utf-8';
+    //Tell PHPMailer to use SMTP
+	$mail->isSMTP();
+	//Enable SMTP debugging
+	// 0 = off (for production use)
+	// 1 = client messages
+	// 2 = client and server messages
+	$mail->SMTPDebug = 2;
+	//Set the hostname of the mail server
+	$mail->Host = 'smtp.gmail.com';
+	// use
+	// $mail->Host = gethostbyname('smtp.gmail.com');
+	// if your network does not support SMTP over IPv6
+	//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+	$mail->Port = '587';
+	//Set the encryption system to use - ssl (deprecated) or tls
+	$mail->SMTPSecure = 'tls';
+	//Whether to use SMTP authentication
+	$mail->SMTPAuth = true;
+	//Username to use for SMTP authentication - use full email address for gmail
+	$mail->Username = "trentmcdole94@gmail.com";
+	//Password to use for SMTP authentication
+	$mail->Password = "mauauyakhakvebex";
+
         //It's important not to use the submitter's address as the from address as it's forgery,
         //which will cause your messages to fail SPF checks.
         //Use an address in your own domain as the from address, put the submitter's address in a reply-to
-        $mail->setFrom('trent@plussumservices.com', $name);
+        $mail->setFrom('trentmcdole94@gmail.com', $name);
         $mail->addReplyTo($email, $name);
         $mail->addAddress('trentmcdole94@gmail.com', 'Trent McDole');
         $mail->Subject = 'Library Suggestion from' . $name;
